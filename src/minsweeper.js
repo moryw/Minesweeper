8a6070db61995124184bf45dbdae9df0092f2021
moryw <matt@skitskat.xyz>
2018-03-14 13:12:51 -0400

    Log what an empty board looks like.

--- src/minsweeper.js
+++ src/minsweeper.js
@@ -1,5 +1,10 @@
 //this is a minesweeper game
 
 
-//this is a hard coded game board line 
+//this is a hard coded game board line
 const blankline = ' | | ';
+
+console.log(`This is what an empty board would look like:`);
+console.log(blankline);
+console.log(blankline);
+console.log(blankline);
