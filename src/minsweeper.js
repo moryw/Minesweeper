e252a5a6a3a19adae05544d5bbefdda10b45e7fc
moryw <matt@skitskat.xyz>
2018-03-22 11:33:08 -0400

    Add printBoard function to log formated arrays

--- src/minsweeper.js
+++ src/minsweeper.js
@@ -1,6 +1,16 @@
 //this is a minesweeper game
+const printBoard = board => {
+  console.log('Current Board: ');
+  console.log(board[0].join(' | '));
+  console.log(board[1].join(' | '));
+  console.log(board[2].join(' | '));
+};
+
 const board = [
   [' ',' ',' '],
   [' ',' ',' '],
   [' ',' ',' ']
 ];
+
+printBoard(board);
+//console.log(board);
