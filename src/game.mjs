1ac8061c91e7bf4815f5bc9b8f83738686079337
moryw <matt@skitskat.xyz>
2018-07-26 17:03:51 +0700

    Add start function for new games and flip function for flipping tiles

--- src/game.mjs
+++ src/game.mjs
@@ -1,4 +1,15 @@
 //game logic for minesweeper
+
+// To play Minesweeper, we will create instances of MineSweeperGame in command line.
+// For example:
+// In the command line, navigate to the lib directory and run `node`
+// Run `.load game.js` to load the contents of this file.
+// Then create a Game instance and run commands like so:
+// let game = new Game(3, 3, 3);
+// game.playMove(0, 1);
+// game.playMove(1, 2);
+// When done run `.exit`
+
 import {Board} from './board.mjs';
 
 class Game {
@@ -23,15 +34,38 @@ class Game {
   }
 };
 
-let game = new Game(3,3,8);
+//let game = new Game(3,3,8);
+let game;
+let start = (rows, colums, bombs) => {
+  game = new Game(rows, colums, bombs);
+  game._board.print();
+};
 
-//console.log(game);
-console.log(`Has safe tiles: ${game._board.hasSafeTiles()}`);
-console.log(`Number of tiles: ${game._board.numberOfTiles}`);
-game.playMove(0,0);
-console.log(`Number of tiles: ${game._board.numberOfTiles}`);
-console.log(`Number of bombs: ${game._board.numberOfBombs}`);
-console.log(`Has safe tiles: ${game._board.hasSafeTiles()}`);
+let flip = (rowIndex, columnIndex) => {
+  game._board.flipTile(rowIndex, columnIndex);
+
+  if (game._board.playerBoard[rowIndex][columnIndex] === 'B') {
+    console.log('You hit a bomb. GAME OVER!');
+    game._board.print();
+  } else if (game._board.hasSafeTiles === true) {
+    console.log('WINNER! There are no more safe tiles.');
+  } else {
+    console.log('Current Board');
+    game._board.print();
+  }
+};
+
+game(3,3,4)
+flip(0,0)
+
+//console.log(game(3,3,4));
+// console.log(game);
+// console.log(`Has safe tiles: ${game._board.hasSafeTiles()}`);
+// console.log(`Number of tiles: ${game._board.numberOfTiles}`);
+//game.playMove(0,0);
+// console.log(`Number of tiles: ${game._board.numberOfTiles}`);
+// console.log(`Number of bombs: ${game._board.numberOfBombs}`);
+// console.log(`Has safe tiles: ${game._board.hasSafeTiles()}`);
 
 
 
