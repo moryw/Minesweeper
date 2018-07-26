e3e0ea14f3307d485c4a6a9a3d3b0c896b1e475c
moryw <matt@skitskat.xyz>
2018-07-26 17:04:26 +0700

    Transpile code for es5

--- src/game.js
+++ src/game.js
@@ -1,4 +1,5 @@
 //game logic for minesweeper
+
 // To play Minesweeper, we will create instances of MineSweeperGame in command line.
 // For example:
 // In the command line, navigate to the lib directory and run `node`
@@ -9,8 +10,13 @@
 // game.playMove(1, 2);
 // When done run `.exit`
 
+// Matts new way to play
+// Run `.load game.js` to load the contents of this file.
+// To start a new game type: start(n,n,n)
+// To flip a tile type: flip(n,n)
+// When done run `.exit`
 
-import {Board} from './board';
+import {Board} from './board.js';
 
 class Game {
   constructor(numberOfRows, numberOfColumns, numberOfBombs) {
@@ -33,3 +39,30 @@ class Game {
     }
   }
 };
+
+//let game = new Game(3,3,8);
+let game;
+let start = (rows, colums, bombs) => {
+  game = new Game(rows, colums, bombs);
+  game._board.print();
+};
+
+let flip = (rowIndex, columnIndex) => {
+  game._board.flipTile(rowIndex, columnIndex);
+
+  if (game._board.playerBoard[rowIndex][columnIndex] === 'B') {
+    console.log('You hit a bomb. GAME OVER!');
+    game._board.print();
+  } else if (game._board.hasSafeTiles === true) {
+    console.log('WINNER! There are no more safe tiles.');
+  } else {
+    console.log('Current Board');
+    game._board.print();
+  }
+};
+
+
+
+
+
+//end
